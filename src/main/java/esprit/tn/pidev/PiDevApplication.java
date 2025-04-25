3953d3cd396816bdcc1f9aa40949c879e252d2ea
khaoula <khaoulahamza641@gmail.com>
2025-04-25 15:44:08 +0100

    prop

--- src/main/java/esprit/tn/pidev/PiDevApplication.java
+++ src/main/java/esprit/tn/pidev/PiDevApplication.java
@@ -9,15 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PiDevApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(PiDevApplication.class, args);
-    }
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE");
-            }};}}
+  public static void main(String[] args) {
+    SpringApplication.run(PiDevApplication.class, args);
+  }
+}
