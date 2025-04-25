a111aad4fc54ab89851800bdfef01958dcca9f60
khaoula <khaoulahamza641@gmail.com>
2025-04-25 09:40:32 +0100

    cnf

--- src/main/java/esprit/tn/pidev/PiDevApplication.java
+++ src/main/java/esprit/tn/pidev/PiDevApplication.java
@@ -1,4 +1,7 @@
 package esprit.tn.pidev;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,5 +12,12 @@ public class PiDevApplication {
     public static void main(String[] args) {
         SpringApplication.run(PiDevApplication.class, args);
     }
-
-}
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:4200")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE");
+            }};}}
