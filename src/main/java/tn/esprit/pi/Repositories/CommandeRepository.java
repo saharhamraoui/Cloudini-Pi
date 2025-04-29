b78d84f0871180d58be5bd8bff5d9cbf9c12cb52
razanesalem <razane.salem@esprit.tn>
2025-04-29 01:25:26 +0100

    add ai

--- src/main/java/tn/esprit/pi/Repositories/CommandeRepository.java
+++ src/main/java/tn/esprit/pi/Repositories/CommandeRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import tn.esprit.pi.entities.Commande;
 
+import java.util.List;
+
 @Repository
 public interface CommandeRepository extends JpaRepository<Commande, Long> {
+    List<Commande> findTop5ByOrderByDateCommandeDesc();
 }
--- src/main/java/tn/esprit/pi/Repositories/MedicamentRepository.java
+++ src/main/java/tn/esprit/pi/Repositories/MedicamentRepository.java
@@ -6,10 +6,12 @@ import tn.esprit.pi.entities.Medicament;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface MedicamentRepository extends JpaRepository<Medicament,Long> {
     List<Medicament> findByDateExpirationBefore(LocalDate date);
-
+    List<Medicament> findByNomContainingIgnoreCase(String nom);
+    Optional<Medicament> findTopByOrderByQuantiteAsc();
 }
 
